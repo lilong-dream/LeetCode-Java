511d0799881e7866c895c664454c72a03ae5f326
lilong_dream <1988lilong@163.com>
2014-04-17 11:34:18 +0800

    generate parentheses

---
+++ Java/GenerateParentheses.java
@@ -0,0 +1,34 @@
+// Problem:  http://oj.leetcode.com/problems/generate-parentheses/
+// Analysis: http://blog.csdn.net/lilong_dream/article/details/23917967
+// 1988lilong@163.com
+
+import java.util.ArrayList;
+
+public class GenerateParentheses {
+	public ArrayList<String> generateParenthesis(int n) {
+		ArrayList<String> res = new ArrayList<String>();
+		generate(n, n, "", res);
+		return res;
+	}
+
+	public void generate(int left, int right, String str, ArrayList<String> res) {
+		if (left == 0 && right == 0) {
+			res.add(str);
+			return;
+		}
+		if (left > 0) {
+			generate(left - 1, right, str + '(', res);
+		}
+		if (right > left) {
+			generate(left, right - 1, str + ')', res);
+		}
+	}
+	
+	public static void main(String[] args) {
+		GenerateParentheses slt = new GenerateParentheses();
+		ArrayList<String> res = slt.generateParenthesis(3);
+		for (String str : res) {
+			System.out.println(str);
+		}
+	}
+}
