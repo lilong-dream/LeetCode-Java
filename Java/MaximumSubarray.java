22dbb45dbadd85a77d02e668484e7c83e78fb8c6
lilong_dream <1988lilong@163.com>
2014-04-18 08:07:22 +0800

    maximum subarray

--- Java/MaximumSubarray.java
+++ Java/MaximumSubarray.java
@@ -1,7 +1,14 @@
 // Author:   Li Long, 1988lilong@163.com
-// Date:     Apr 17, 2014
+// Date:     Apr 18, 2014
 // Source:   http://oj.leetcode.com/problems/maximum-subarray/
 // Analysis: http://blog.csdn.net/lilong_dream/article/details/19846683
+
+// Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+// For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+// the contiguous subarray [4,-1,2,1] has the largest sum = 6. 
+// More practice:
+// If you have figured out the O(n) solution, 
+// try coding another solution using the divide and conquer approach, which is more subtle.
 
 public class MaximumSubarray {
 	public int maxSubArray(int[] A) {
