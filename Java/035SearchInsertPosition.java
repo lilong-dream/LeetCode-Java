4e481a631e668b5b4abd3dbc70c5f58267f64da2
Li Long <1988lilong@163.com>
2015-01-24 12:31:20 +0800

    Rename SearchInsertPosition.java to 035SearchInsertPosition.java

--- Java/035SearchInsertPosition.java
+++ Java/035SearchInsertPosition.java
@@ -8,10 +8,10 @@
 // If not, return the index where it would be if it were inserted in order.
 // You may assume no duplicates in the array.
 // Here are few examples.
-// [1,3,5,6], 5 ¡ú 2
-// [1,3,5,6], 2 ¡ú 1
-// [1,3,5,6], 7 ¡ú 4
-// [1,3,5,6], 0 ¡ú 0 
+// [1,3,5,6], 5 Â¡Ãº 2
+// [1,3,5,6], 2 Â¡Ãº 1
+// [1,3,5,6], 7 Â¡Ãº 4
+// [1,3,5,6], 0 Â¡Ãº 0 
 
 public class SearchInsertPosition {
 	public int searchInsert(int[] A, int target) {
